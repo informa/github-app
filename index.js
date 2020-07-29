02799f7e8b73af31c266bcbd8bd406a2e6bee6bd
Jon Price <jon.price77@gmail.com>
2020-07-29 21:44:30 +1000

    get commit hash, get files from tree

--- index.js
+++ index.js
@@ -7,16 +7,48 @@ module.exports = (app) => {
   app.log("Yay, the app was loaded!");
 
   app.on("issues.opened", async (context) => {
-    // app.log(
-    //   "context",
-    //   context.payload.sender.login,
-    //   context.payload.repository.name
-    // );
+    app.log("context", context);
 
+    const owner = context.payload.sender.login;
+    const repo = context.payload.repository.name;
+
+    // Get files
+    const getfiles = (sha) => {
+      context.github.git
+        .getTree({
+          owner,
+          repo,
+          tree_sha: sha,
+        })
+        .then((tree) => {
+          tree.data.tree.map((branch) => {
+            const { path, type, sha } = branch;
+            if (type === "blob") {
+              console.log("PATH: ", path);
+            }
+            if (type === "tree") {
+              getfiles(sha);
+            }
+          });
+        });
+    };
+
+    // Get sha from latest commit
+    context.github.repos
+      .listCommits({
+        owner,
+        repo,
+      })
+      .then((result) => {
+        const sha = result.data[0].sha;
+        getfiles(sha);
+      });
+
+    // Look at file
     context.github.repos
       .getContents({
-        owner: context.payload.sender.login,
-        repo: context.payload.repository.name,
+        owner,
+        repo,
         path: "package.json",
       })
       .then((result) => {
