e48a19f03f8cd238a20a0bcee240e68516d49688
Jon Price <jon.price77@gmail.com>
2020-07-31 11:59:38 +1000

    added scheduler to repo, retreived sha from branch

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
+const createScheduler = require("probot-scheduler");
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
-module.exports = (app) => {
+module.exports = (robot) => {
   // Your code here
-  app.log("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    app.log("context", context);
-
-    const owner = context.payload.sender.login;
+  createScheduler(robot, {
+    interval: 20000,
+  });
+  robot.on("schedule.repository", (context) => {
     const repo = context.payload.repository.name;
+    const owner = context.payload.repository.owner.login;
+    // master
+    const default_branch = context.payload.repository.default_branch;
+
+    let files = [];
+
+    console.log("REPO: ", repo, "OWNER: ", owner);
+
+    // Get sha from the default branch and run get files
+    context.github.repos
+      .getBranch({
+        owner,
+        repo,
+        branch: default_branch,
+      })
+      .then((result) => {
+        const sha = result.data.commit.sha;
+
+        getfiles(sha);
+      });
+
+    const addPathToArray = (path) => {
+      files.push(path);
+    };
 
     // Get files
     const getfiles = (sha) => {
@@ -23,28 +48,23 @@ module.exports = (app) => {
         .then((tree) => {
           tree.data.tree.map((branch) => {
             const { path, type, sha } = branch;
-            if (type === "blob") {
-              console.log("PATH: ", path);
+            if (type === "blob" && path.includes('.js')) {
+              addPathToArray(path);
             }
             if (type === "tree") {
               getfiles(sha);
             }
           });
+        })
+        .catch((error) => {
+          console.error("ERROR: ", error);
+        })
+        .finally(() => {
+          console.log("FILES: ", files);
         });
     };
 
-    // Get sha from latest commit
-    context.github.repos
-      .listCommits({
-        owner,
-        repo,
-      })
-      .then((result) => {
-        const sha = result.data[0].sha;
-        getfiles(sha);
-      });
-
-    // Look at file
+    // Get the package.json and console the react version
     context.github.repos
       .getContents({
         owner,
@@ -64,12 +84,13 @@ module.exports = (app) => {
         console.log(reactVersion);
       });
 
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.github.issues.createComment(issueComment);
-  });
 
+      // Workflow
+      // 1. does repo have package.json ?
+      // 2. does package.json have react ? react version
+      // 3. look for files (js,jsx) don't look in folder like (public,test) - list of filename
+      // 4. loop over files and look and the contents of the the file.
+  });
   // For more information on building apps:
   // https://probot.github.io/docs/
 
