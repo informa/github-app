11a5276dd97d30e4f5ac108f53ec36ba337ed4fa
Jon Price <jon.price77@gmail.com>
2020-08-03 22:07:26 +1000

    added option to clone repos

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const createScheduler = require("probot-scheduler");
+const Git = require("nodegit");
 
 /**
  * This is the main entrypoint to your Probot app
@@ -47,54 +48,70 @@ module.exports = (app) => {
       return;
     }
 
+    // 4. create JWT and clone repo
+    const github = await app.auth(); // Not passing an id returns a JWT-authenticated client
+    const token = await github.apps.createInstallationToken({
+      installation_id: context.payload.installation.id,
+    });
+
+    Git.Clone(
+      `https://x-access-token:${token.data.token}@github.com/${login}/${repoName}.git`,
+      `repos/${repoName}`
+    ).then(function (repository) {
+      // Work with the repository object here.
+    });
+
+    // OR build file names using https://developer.github.com/v3/git/trees/ and scan them.
+
+
     // 4. As react (myob-widgets) is installed we want to get the files react (myob-widgts) is included
     // First we need to get the Get Branch as to get the sha
-    const defaultBranch = context.payload.repository.default_branch;
+    // const defaultBranch = context.payload.repository.default_branch;
 
-    const branch = await context.github.request(
-      `GET /repos/${login}/${repoName}/branches/${defaultBranch}`
-    );
+    // const branch = await context.github.request(
+    //   `GET /repos/${login}/${repoName}/branches/${defaultBranch}`
+    // );
 
-    // Get tree: A Git tree object creates the hierarchy between files in a Git repository
-    // https://developer.github.com/v3/git/trees/
-    const readRepo = async ({ login, repoName, sha }) =>
-      await context.github.request(
-        `GET /repos/${login}/${repoName}/git/trees/${sha}`
-      );
+    // // Get tree: A Git tree object creates the hierarchy between files in a Git repository
+    // // https://developer.github.com/v3/git/trees/
+    // const readRepo = async ({ login, repoName, sha }) =>
+    //   await context.github.request(
+    //     `GET /repos/${login}/${repoName}/git/trees/${sha}`
+    //   );
 
-    const readFiles = (files, path) =>
-      files.data.tree.forEach(async (file) => {
-        if (file.type === "blob" && file.path.match(/^.*\.(js|jsx|ts)$/)) {
-          // 5. If the file (blob) is a js,jsx,ts file then we need to see if if contains react (myob-widgets)
-          await context.github.request(
-            `GET /repos/${login}/${repoName}/contents/${path.join("/")}/${
-              file.path
-            }`
-          );
-          console.log("FILENAME: ", `${path.join("/")}/${file.path}`, repoName);
-        }
+    // const readFiles = (files, path) =>
+    //   files.data.tree.forEach(async (file) => {
+    //     if (file.type === "blob" && file.path.match(/^.*\.(js|jsx|ts)$/)) {
+    //       // 5. If the file (blob) is a js,jsx,ts file then we need to see if if contains react (myob-widgets)
+    //       await context.github.request(
+    //         `GET /repos/${login}/${repoName}/contents/${path.join("/")}/${
+    //           file.path
+    //         }`
+    //       );
+    //       console.log("FILENAME: ", `${path.join("/")}/${file.path}`, repoName);
+    //     }
 
-        // If the file is a directory (tree) then we need to send another request
-        // But if the directory is test then don't bother
-        if (file.type === "tree" && file.path.indexOf("test") < 0) {
-          const filesInTree = await readRepo({
-            login,
-            repoName,
-            sha: file.sha,
-          });
+    //     // If the file is a directory (tree) then we need to send another request
+    //     // But if the directory is test then don't bother
+    //     if (file.type === "tree" && file.path.indexOf("test") < 0) {
+    //       const filesInTree = await readRepo({
+    //         login,
+    //         repoName,
+    //         sha: file.sha,
+    //       });
 
-          readFiles(filesInTree, path.concat([file.path]));
-        }
-      });
+    //       readFiles(filesInTree, path.concat([file.path]));
+    //     }
+    //   });
 
-    readFiles(
-      await readRepo({
-        login,
-        repoName,
-        sha: branch.data.commit.sha,
-      }),
-      []
-    );
+    // readFiles(
+    //   await readRepo({
+    //     login,
+    //     repoName,
+    //     sha: branch.data.commit.sha,
+    //   }),
+    //   []
+    // );
   });
   // For more information on building apps:
   // https://probot.github.io/docs/
